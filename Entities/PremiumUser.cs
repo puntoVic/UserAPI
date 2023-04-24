dcdbdc59b4518c0923f1c45460e9869081ba71c6
Victor Olvera <volvera.thome@gmail.com>
2023-04-24 09:52:49 -0600

    Entities

---
+++ Entities/PremiumUser.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class PremiumUser : IUser
+    {
+        private string name;
+        private string email;
+        private string address;
+        private string phone;
+        private string userType;
+        private decimal money;
+
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string Address { get => address; set => address = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Type { get => userType; set => userType = value; }
+        public decimal Money { get => money; set => money = value; }
+        
+
+        public void AddGift()
+        {
+            if (Money > 100)
+            {
+                var gif = Money * 2;
+                Money += gif;
+            }
+        }
+    }
+}
