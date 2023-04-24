81ff131ba7fe1934634477f99a9a9766642decf6
Victor Olvera <volvera.thome@gmail.com>
2023-04-24 09:55:59 -0600

    Common library

---
+++ Entities/Definitions/DefinitionHelper.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Interfaces;
+
+namespace Entities.Definitions
+{
+    public static class DefinitionHelper
+    {
+        public static IUser ToUser(this UserDefinition definition)
+        {
+            IUser newUser = definition?.Type.ToLower() switch
+            {
+                "normal" => new NormalUser(),
+                "nuperUser" => new SuperUser(),
+                "premium" => new PremiumUser(),
+                _ => null,
+            };
+            if (newUser != null)
+            {
+                newUser.Name = definition.Name;
+                newUser.Email = definition.Email;
+                newUser.Address = definition.Address;
+                newUser.Phone = definition.Phone;
+                newUser.Type = definition.Type;
+                newUser.Money = definition.Money;
+            }
+            return newUser ?? null;
+
+        }
+    }
+}
