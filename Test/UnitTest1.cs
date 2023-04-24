4d2504c806142b988cc3e25466571ad207e89a64
Victor Olvera <volvera.thome@gmail.com>
2023-04-24 10:09:16 -0600

    Test correction

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,33 +1,62 @@
-using System;
+using Data.DataAccess;
+using Data;
+using Entities.Definitions;
 using UserAPI.Controllers;
 using Xunit;
+using Business;
 
 namespace Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async void Test1()
         {
-            //var userController = new UsersController();
 
-            //var result = userController.CreateUser("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
+            var dataContex = new DataContext("\\Files\\Users.txt");
+            var userDataAcces = new UserDataAccess(dataContex);
+            var userManager = new UserManager(userDataAcces);
+
+            var user = new UserDefinition()
+            {
+                Name = "Mike",
+                Email = "mike@gmail.com",
+                Address = "Av. Juan G",
+                Phone = "+349 1122354215",
+                Type = "Normal",
+                Money = 124
+            };
+            var userController = new UsersController(userManager);
+
+            var result = await userController.CreateUser(user);
 
 
-            //Assert.Equal(true, result.IsSuccess);
-            //Assert.Equal("User Created", result.Errors);
+            Assert.True(result.IsSuccess);
+            Assert.Equal("User Created", result.Errors);
         }
 
         [Fact]
         public void Test2()
         {
-            //var userController = new UsersController();
+            var dataContex = new DataContext("\\Files\\Users.txt");
+            var userDataAcces = new UserDataAccess(dataContex);
+            var userManager = new UserManager(userDataAcces);
+            var userController = new UsersController(userManager);
+            var user = new UserDefinition()
+            {
+                Name = "Agustina",
+                Email = "Agustina@gmail.com",
+                Address = "Av. Juan G",
+                Phone = "+534645213542",
+                Type = "Normal",
+                Money = 124
+            };
 
-            //var result = userController.CreateUser("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
+            var result = userController.CreateUser(user).Result;
 
 
-            //Assert.Equal(false, result.IsSuccess);
-            //Assert.Equal("The user is duplicated", result.Errors);
+            Assert.False(result.IsSuccess);
+            Assert.Equal("The user is duplicated", result.Errors);
         }
     }
 }
